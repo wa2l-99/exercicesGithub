b8537f2b5c5c1e77ed08d537732af63095673464
wa2l-99 <waelzantour1@gmail.com>
2024-05-14 13:58:59 +0200

    C1 : ajout du composant Helloworld

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWordComponent } from './pages/hello-word/hello-word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/hello-word/hello-word.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-word',
+  templateUrl: './hello-word.component.html',
+  styleUrl: './hello-word.component.scss'
+})
+export class HelloWordComponent {
+
+}
