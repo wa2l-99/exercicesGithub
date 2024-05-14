cb94571f946a851d8cd60e63dc973791021e1dc5
wa2l-99 <waelzantour1@gmail.com>
2024-05-14 19:02:13 +0200

    ajout d'un commentiare controled by process  Tue May 14 19:02:13 2024

--- src/app/pages/hello-word/hello-word.component.ts
+++ src/app/pages/hello-word/hello-word.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './hello-word.component.html',
   styleUrl: './hello-word.component.scss'
 })
-export class HelloWordComponent {
+export class HelloWordComponent { 
+  //hellowrold message 
+
   msg : string = "Hello world from bordeaux"
 
   constructor(){}
