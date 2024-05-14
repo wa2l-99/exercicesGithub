f051e29370f2575be1257a74a44f83df30a171bf
wa2l-99 <waelzantour1@gmail.com>
2024-05-14 14:25:01 +0200

    C5: fond ecran en jaune

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent {
   title = 'hello-world';
